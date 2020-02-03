0bb0cd95dc7d5994a5ab7a6332db2a1e7d66c297
Mike Looijmans <mike.looijmans@topic.nl>
2020-02-03 11:02:07 +0100

    Proper alignment and resize of the floorplan image
    Changing the floorplan didn't really work well, fix the alignment settings
    so that the widgets automatically adjust themselves and make the default
    layouts match that.

--- dyplocontext.cpp
+++ dyplocontext.cpp
@@ -38,21 +38,21 @@ static void use7030config(QVector<DyploNodeInfo> *result)
     result->push_back(DyploNodeInfo(0, QRect(110,  90, 91, 41), DyploNodeInfo::CPU));
     result->push_back(DyploNodeInfo(1, QRect(110, 140, 91, 41), DyploNodeInfo::CPU));
 
-    result->push_back(DyploNodeInfo(2, QRect(330, 430, 71, 81), DyploNodeInfo::FIXED, "stream_mux"));
-    result->push_back(DyploNodeInfo(3, QRect(330, 520, 71, 81), DyploNodeInfo::FIXED, "stream_mux"));
+    result->push_back(DyploNodeInfo(2, QRect(330, 424, 71, 81), DyploNodeInfo::FIXED, "stream_mux"));
+    result->push_back(DyploNodeInfo(3, QRect(330, 514, 71, 81), DyploNodeInfo::FIXED, "stream_mux"));
 
-    result->push_back(DyploNodeInfo(4, QRect(210, 35, 110, 192), DyploNodeInfo::PR));
-    result->push_back(DyploNodeInfo(5, QRect(210, 227, 110, 192), DyploNodeInfo::PR));
-    result->push_back(DyploNodeInfo(6, QRect(450, 35, 120, 192), DyploNodeInfo::PR));
-    result->push_back(DyploNodeInfo(7, QRect(450, 227, 120, 192), DyploNodeInfo::PR));
-    result->push_back(DyploNodeInfo(8, QRect(450, 419, 120, 192), DyploNodeInfo::PR));
-    result->push_back(DyploNodeInfo(9, QRect(426, 613, 142, 192), DyploNodeInfo::PR));
-    result->push_back(DyploNodeInfo(10, QRect(184, 613, 120, 192), DyploNodeInfo::PR));
-    result->push_back(DyploNodeInfo(11, QRect(50, 613, 114, 192), DyploNodeInfo::PR));
+    result->push_back(DyploNodeInfo(4, QRect(210, 29, 110, 192), DyploNodeInfo::PR));
+    result->push_back(DyploNodeInfo(5, QRect(210, 221, 110, 192), DyploNodeInfo::PR));
+    result->push_back(DyploNodeInfo(6, QRect(450, 29, 120, 192), DyploNodeInfo::PR));
+    result->push_back(DyploNodeInfo(7, QRect(450, 221, 120, 192), DyploNodeInfo::PR));
+    result->push_back(DyploNodeInfo(8, QRect(450, 413, 120, 192), DyploNodeInfo::PR));
+    result->push_back(DyploNodeInfo(9, QRect(426, 607, 142, 192), DyploNodeInfo::PR));
+    result->push_back(DyploNodeInfo(10, QRect(184, 607, 120, 192), DyploNodeInfo::PR));
+    result->push_back(DyploNodeInfo(11, QRect(50, 607, 114, 192), DyploNodeInfo::PR));
 
-    result->push_back(DyploNodeInfo(12, QRect(110, 240, 91, 41), DyploNodeInfo::DMA));
-    result->push_back(DyploNodeInfo(13, QRect(110, 290, 91, 41), DyploNodeInfo::DMA));
-    result->push_back(DyploNodeInfo(14, QRect(110, 340, 91, 41), DyploNodeInfo::DMA));
+    result->push_back(DyploNodeInfo(12, QRect(110, 260, 91, 41), DyploNodeInfo::DMA));
+    result->push_back(DyploNodeInfo(13, QRect(110, 310, 91, 41), DyploNodeInfo::DMA));
+    result->push_back(DyploNodeInfo(14, QRect(110, 360, 91, 41), DyploNodeInfo::DMA));
 }
 
 /* Nodes in logic 7015:
@@ -65,13 +65,13 @@ static void use7015config(QVector<DyploNodeInfo> *result)
 {
     result->push_back(DyploNodeInfo(0, QRect(110,  90, 91, 41), DyploNodeInfo::CPU));
 
-    result->push_back(DyploNodeInfo(1, QRect(210, 35, 110, 192), DyploNodeInfo::PR));
-    result->push_back(DyploNodeInfo(2, QRect(210, 227, 110, 192), DyploNodeInfo::PR));
-    result->push_back(DyploNodeInfo(3, QRect(450, 35, 120, 192), DyploNodeInfo::PR));
-    result->push_back(DyploNodeInfo(4, QRect(450, 227, 120, 192), DyploNodeInfo::PR));
+    result->push_back(DyploNodeInfo(1, QRect(210, 29, 110, 192), DyploNodeInfo::PR));
+    result->push_back(DyploNodeInfo(2, QRect(210, 221, 110, 192), DyploNodeInfo::PR));
+    result->push_back(DyploNodeInfo(3, QRect(450, 29, 120, 192), DyploNodeInfo::PR));
+    result->push_back(DyploNodeInfo(4, QRect(450, 221, 120, 192), DyploNodeInfo::PR));
 
-    result->push_back(DyploNodeInfo(5, QRect(110, 240, 91, 41), DyploNodeInfo::DMA));
-    result->push_back(DyploNodeInfo(6, QRect(110, 290, 91, 41), DyploNodeInfo::DMA));
+    result->push_back(DyploNodeInfo(5, QRect(110, 260, 91, 41), DyploNodeInfo::DMA));
+    result->push_back(DyploNodeInfo(6, QRect(110, 310, 91, 41), DyploNodeInfo::DMA));
 }
 
 DyploContext::DyploContext():
